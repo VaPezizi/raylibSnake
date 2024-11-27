aee1fb40aec3f28be964fe77a11ef5afed61d46a
Samuli Pihlaja <samuli.pihlaja.SP@gmail.com>
2024-11-27 22:43:13 +0200

    Well i guess i got the list structure working, but the game is still really early and buggy

--- main.c
+++ main.c
@@ -2,13 +2,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//Some variables
 const int screenWidth = 800;
 const int screenHeight = 800;
 
 const int cellCount = 40;
 const int cellSize = 20;
 
-//Structs
+
+//Structs and enums
+
+enum DIRECTION{
+	UP,
+	DOWN,
+	LEFT,
+	RIGHT
+};
 
 typedef struct SnakePart{
 	int posX;
@@ -19,6 +28,7 @@ typedef struct SnakePart{
 typedef struct{
 	SnakePart * HEAD;
 	int size;
+	int direction;
 	int score;
 }Snake;
 
@@ -31,28 +41,78 @@ void * safeMalloc(size_t n){
 	return p;
 }
 
-void addPart(const int posY, const int posX, Snake * snake){
+void moveNode(Snake * snake){
 	if(snake->HEAD == NULL){
-		snake->HEAD = malloc(sizeof(SnakePart));
+		fprintf(stderr, "Error in snake moving");
+		abort();
+	}
+	if(snake->score < 3) return;
+	SnakePart * current = snake->HEAD;
+
+	while(current->next->next != NULL){
+		current = current->next;
+	}
+	current->next->next = snake->HEAD;
+	snake->HEAD = current->next;
+	current->next = NULL;
+
+		
+}
+
+//Returns -1 if the move is "Illegal" (Meaning if you would lose the game)
+int moveSnake(Snake * snake, int direction){
+	
+	if(snake->HEAD == NULL) return -1;	
+	
+	moveNode(snake);
+	switch(direction){
+		case RIGHT:
+			snake->HEAD->posX += cellSize;	
+			//snake->TA
+			break;
+		case LEFT:
+			snake->HEAD->posX -= cellSize;	
+			break;
+		case UP:
+			snake->HEAD->posY += cellSize;
+			break;
+		case DOWN:
+			snake->HEAD->posY -= cellSize;
+			break;
+		default:
+			fprintf(stderr, "ERROR IN SNAKE MOVING");
+	}	
+
+	return 1;
+}
+
+void addPart(const int posY, const int posX, Snake * snake){
+	snake->score++;
+	if(snake->HEAD == NULL){
+		snake->HEAD = safeMalloc(sizeof(SnakePart));
 		snake->HEAD->next = NULL;
 		snake->HEAD->posX = posX;
 		snake->HEAD->posY = posY;
-	}else{
-		SnakePart * temp = snake->HEAD;
+	}
+	else{
+		/*SnakePart * temp = snake->HEAD;
 		while(temp->next != NULL){
 			temp = temp->next;
 		}
-		snake->score++;
-		temp->next = safeMalloc(sizeof(SnakePart));
-		temp->next->next = NULL;
-		temp->next->posX = posX;
-		temp->next->posY = posY;
+		snake->score++;*/
+		SnakePart * temp = safeMalloc(sizeof(SnakePart));
+		temp->next = snake->HEAD;
+		temp->posX = posX;
+		temp->posY = posY;
+		snake->HEAD = temp;
 		return;
+		
 	}	
 	return;	
 }
 void drawSnake(Snake * snake){
 	if(snake->HEAD == NULL){
+		
 		fprintf(stderr, "Snake not initialized!!");
 		abort();
 	}
@@ -83,21 +143,31 @@ void freeSnake(Snake * snake){
 
 
 
-int main(){
-	Snake snake = {NULL, cellSize, 0};
-	for(int i = 0; i < 10; i++){
+int main(){	
+	Snake snake = {NULL, cellSize, RIGHT, 0};
+	for(int i = 0; i < 5; i++){
 	       	addPart(20 * cellSize, (20 - i) *cellSize, &snake);	
 	
 	}
+
+	int direction = RIGHT;
+
 	InitWindow(screenWidth, screenHeight, "Raylib Snake");		
 	ClearBackground(WHITE);
-	SetTargetFPS(60);
+	SetTargetFPS(30);
 	
 	while(!WindowShouldClose()){
-		BeginDrawing();
-		
+		if(IsKeyPressed(KEY_S))direction = DOWN;
+		else if(IsKeyPressed(KEY_W))direction = UP;
+		else if(IsKeyPressed(KEY_A))direction = LEFT;
+		else if(IsKeyPressed(KEY_D))direction = RIGHT;
+
+		moveSnake(&snake, direction);
+
+		BeginDrawing();	
+		drawSnake(&snake);
 		for(int i = 0; i < 41;i++){
-			drawSnake(&snake);	
+
 			DrawLineEx((Vector2){cellSize * i, 0},(Vector2){cellSize * i, screenHeight}, 2, BLACK);
 			DrawLineEx((Vector2){0, i*cellSize}, (Vector2){screenWidth, i*cellSize}, 2, BLACK);	
 		}
